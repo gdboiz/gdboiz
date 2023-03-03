5d01ba015f0b1e29fc09bd3f1d29ced83495efbc
Ryu <80870887+SeungwonRyu@users.noreply.github.com>
2023-03-03 17:03:12 +0900

    23.03.03 (Sean)  - 초기 커밋 (Spring Boot)

---
+++ src/main/java/com/example/gdboiz/GdboizApplication.java
@@ -0,0 +1,13 @@
+package com.example.gdboiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdboizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdboizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gdboiz/GdboizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gdboiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdboizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
