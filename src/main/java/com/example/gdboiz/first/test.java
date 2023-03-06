5cc5eed4688d43e08fe26124765797ebddafc637
CHANGH0 <chlckdgh98@naver.com>
2023-03-06 17:33:00 +0900

    응애 애기 restapi

--- src/main/java/com/example/gdboiz/first/test.java
+++ src/main/java/com/example/gdboiz/first/test.java
@@ -1,4 +1,23 @@
 package com.example.gdboiz.first;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 public class test {
+    @GetMapping("/first/{searchData}")
+    public ResponseEntity searchData(@PathVariable String searchValue){
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("data1", "value1");
+        data.put("data2", "value2");
+        data.put("data2", "value3");
+
+        return ResponseEntity.ok(data);
+    }
 }
